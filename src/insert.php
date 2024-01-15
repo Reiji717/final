d70953c539c00acadba6274537b1cd8b8a1039bb
Reiji717 <2201192@s.asojuku.ac.jp>
2024-01-15 12:21:07 +0900

    up

---
+++ src/insert.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>登録</title>
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+<body>
+<h2>楽曲を追加</h2>
+<a href="menu.php">メニューに戻る</a>
+<hr>
+<form action="" method="post">
+    食品名：<input type="text" name="food">
+    <br>
+    カロリー：<input type="text" name="calorie">
+    <br>
+    <button type="submit" name="action" value="send">登録</button>
+    <br>
+</form>
+</body>
+</html>
---
+++ src/list.php
@@ -0,0 +1,14 @@
+<?php session_start();?>
+<?php require 'db-connect.php'; ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>食品一覧</title>
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+<body>
+<h2>食品一覧</h2>
+<a href="CRUD-menu.php">メニューに戻る</a>
+<hr>
+</html>
