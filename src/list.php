f9dc577134b50ee42343470ca812b9721460574d
Reiji717 <2201192@s.asojuku.ac.jp>
2024-01-16 10:52:58 +0900

    up

--- src/list.php
+++ src/list.php
@@ -10,5 +10,17 @@
 <body>
 <h2>食品一覧</h2>
 <a href="CRUD-menu.php">メニューに戻る</a>
-<hr>
 </html>
+
+<?php
+    const DBNAME = 'LAA1517435-final';
+    $pdo=new PDO('mysql:host=mysql220.phy.lolipop.lan;dbname=LAA1517435-final;charset=utf8','','Pass0717')
+    
+    foreach ($pdo->query('select * from food')as $row){
+        echo '<p>';
+        echo $row['food_id'], ':';
+        echo $row['food_mei'], ':';
+        echo $row['calorie'];
+        echo '</p>';
+    }
+    ?>
